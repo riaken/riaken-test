c9f7ecfb9691e0862225fbc95911c11ae39b5fde
Brian Jones <mojobojo@gmail.com>
2013-12-02 14:54:30 +0900

    go 1.2 fixes, better concurrency test coverage

--- rc/rc_integration.go
+++ rc/rc_integration.go
@@ -4,18 +4,20 @@ import (
 	"log"
 	"runtime"
 	"strconv"
-	"time"
 )
 
 import (
 	riaken "github.com/riaken/riaken-core"
 )
 
+const LIMIT int = 1000
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 
 	addrs := []string{"127.0.0.1:8083", "127.0.0.1:8084", "127.0.0.1:8085", "127.0.0.1:8086", "127.0.0.1:8087"}
-	client := riaken.NewClient(addrs, 20, time.Second*2)
+	client := riaken.NewClient(addrs, 10)
+	//client.Debug(true)
 	defer client.Close()
 	client.Dial()
 
@@ -23,34 +25,44 @@ func main() {
 
 	i := 0
 	for {
-		go func() {
-			session, err := client.Session()
-			if err != nil {
-				log.Print(err.Error())
-				return
-			}
-			defer session.Close()
+		sem := make(chan bool, LIMIT)
+		for t := 0; t < LIMIT; t++ {
+			go func(i int) {
+				defer func() {
+					sem <- true
+				}()
+				session := client.Session()
+				defer session.Release()
 
-			id := strconv.FormatInt(int64(i), 10)
-			bucket := session.GetBucket("rc-animals")
-			object := bucket.Object("id-" + id)
+				id := strconv.FormatInt(int64(i), 10)
+				bucket := session.GetBucket("rc-animals")
+				object := bucket.Object("id-" + id)
 
-			log.Printf("executing: %d", i)
+				if _, err := object.Store([]byte("animal" + id)); err != nil {
+					log.Print(err.Error())
+					return
+				}
 
-			if _, err := object.Store([]byte("animal" + id)); err != nil {
-				log.Print(err.Error())
-			}
+				if _, err := object.Fetch(); err != nil {
+					log.Print(err.Error())
+					return
+				}
 
-			if _, err := object.Fetch(); err != nil {
-				log.Print(err.Error())
-			}
+				if _, err := object.Delete(); err != nil {
+					log.Print(err.Error())
+					return
+				}
 
-			if _, err := object.Delete(); err != nil {
-				log.Print(err.Error())
-			}
+				if i%100 == 0 {
+					log.Printf("executing: %d", i)
+				}
 
+			}(i)
 			i++
-		}()
+		}
+		for t := 0; t < LIMIT; t++ {
+			<-sem
+		}
 	}
 
 	<-done
--- rs/rs_integration.go
+++ rs/rs_integration.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"runtime"
 	"strconv"
-	"time"
 )
 
 import (
 	riaken "github.com/riaken/riaken-struct"
 )
+
+const LIMIT int = 1000
 
 type Animal struct {
 	Name string `json:"name"`
@@ -20,7 +21,8 @@ func main() {
 
 	marshaller := riaken.NewStructMarshal("json", riaken.JsonMarshaller, riaken.JsonUnmarshaller)
 	addrs := []string{"127.0.0.1:8083", "127.0.0.1:8084", "127.0.0.1:8085", "127.0.0.1:8086", "127.0.0.1:8087"}
-	client := riaken.NewClient(addrs, 20, time.Second*2, marshaller)
+	client := riaken.NewClient(addrs, 10, marshaller)
+	//client.Debug(true)
 	defer client.Close()
 	client.Dial()
 
@@ -28,42 +30,52 @@ func main() {
 
 	i := 0
 	for {
-		go func() {
-			session, err := client.Session()
-			if err != nil {
-				log.Print(err.Error())
-				return
-			}
-			defer session.Close()
+		sem := make(chan bool, LIMIT)
+		for t := 0; t < LIMIT; t++ {
+			go func(i int) {
+				defer func() {
+					sem <- true
+				}()
+				session := client.Session()
+				defer session.Release()
 
-			id := strconv.FormatInt(int64(i), 10)
-			bucket := session.GetBucket("rs-animals")
-			object := bucket.Object("id-" + id)
-			animal := &Animal{
-				Name: "animal" + id,
-			}
+				id := strconv.FormatInt(int64(i), 10)
+				bucket := session.GetBucket("rs-animals")
+				object := bucket.Object("id-" + id)
+				animal := &Animal{
+					Name: "animal" + id,
+				}
 
-			log.Printf("executing: %d", i)
+				if _, err := object.Store(animal); err != nil {
+					log.Print(err.Error())
+					return
+				}
 
-			if _, err := object.Store(animal); err != nil {
-				log.Print(err.Error())
-			}
+				var check Animal
+				if _, err := object.Fetch(&check); err != nil {
+					log.Print(err.Error())
+					return
+				}
 
-			var check Animal
-			if _, err := object.Fetch(&check); err != nil {
-				log.Print(err.Error())
-			}
+				if check.Name != animal.Name {
+					log.Print("name mismatch!")
+				}
 
-			if check.Name != animal.Name {
-				log.Print("name mismatch!")
-			}
+				if _, err := object.Delete(); err != nil {
+					log.Print(err.Error())
+					return
+				}
 
-			if _, err := object.Delete(); err != nil {
-				log.Print(err.Error())
-			}
+				if i%100 == 0 {
+					log.Printf("executing: %d", i)
+				}
 
+			}(i)
 			i++
-		}()
+		}
+		for t := 0; t < LIMIT; t++ {
+			<-sem
+		}
 	}
 
 	<-done
